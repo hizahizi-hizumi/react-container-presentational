bec30b75952b21a558a67093ad783072fa8d9d46
hizahizi-hizumi <hizahizihizumi@gmail.com>
2024-08-03 18:21:53 +0900

    feat: タスク作成機能追加

--- front/src/pages/tasks/index.tsx
+++ front/src/pages/tasks/index.tsx
@@ -1,9 +1,39 @@
 import type React from "react";
 
+import { useForm } from "react-hook-form";
+import type { Arguments } from "swr";
+import useSWRMutation from "swr/mutation";
+
 import { useTasks } from "./hooks/useTasks";
 
 export default function Tasks(): React.JSX.Element {
   const { tasks, error, isLoading } = useTasks();
+
+  type Inputs = {
+    title: string;
+  };
+
+  const { register, handleSubmit } = useForm<Inputs>();
+
+  async function createTask(url: string, { arg }: { arg: Arguments }) {
+    const res = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(arg),
+    });
+
+    return res.json();
+  }
+
+  const { trigger, isMutating } = useSWRMutation("/tasks", createTask);
+
+  function onSubmit(data: Inputs) {
+    trigger(data);
+  }
+
+  const canSubmit = !isMutating;
 
   if (isLoading) {
     return <p>Loading...</p>;
@@ -15,6 +45,13 @@ export default function Tasks(): React.JSX.Element {
   return (
     <>
       <h1>Tasks</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label>Title</label>
+        <input {...register("title")} />
+        <button type="submit" disabled={!canSubmit}>
+          作成
+        </button>
+      </form>
       <ul>
         {tasks.map((task) => (
           <li key={task.id}>{task.title}</li>
