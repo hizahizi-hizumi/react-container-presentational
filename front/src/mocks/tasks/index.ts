5f7cee82cf975555856190b7a9cfca208881ac2a
hizahizi-hizumi <hizahizihizumi@gmail.com>
2024-08-03 16:54:37 +0900

    feat: タスク作成のエンドポンとモック作成

--- front/src/mocks/tasks/index.ts
+++ front/src/mocks/tasks/index.ts
@@ -9,16 +9,50 @@ type GetTasksResponseBody = {
   tasks: Task[];
 };
 
-export const handlers = [
-  http.get<GetTasksParams, GetTasksRequestBody, GetTasksResponseBody, "/tasks">(
-    "/tasks",
-    () => {
-      return HttpResponse.json({
-        tasks: [
-          { id: 1, title: "Task 1" },
-          { id: 2, title: "Task 2" },
-        ],
-      });
-    },
-  ),
+const allTasks: Task[] = [
+  { id: 1, title: "Task 1" },
+  { id: 2, title: "Task 2" },
 ];
+
+const get = http.get<
+  GetTasksParams,
+  GetTasksRequestBody,
+  GetTasksResponseBody,
+  "/tasks"
+>("/tasks", () => {
+  return HttpResponse.json({
+    tasks: allTasks,
+  });
+});
+
+type PostTasksParams = Record<string, never>;
+
+type PostTasksRequestBody = {
+  title: string;
+};
+
+type PostTasksResponseBody = {
+  task: Task;
+};
+
+const post = http.post<
+  PostTasksParams,
+  PostTasksRequestBody,
+  PostTasksResponseBody,
+  "/tasks"
+>("/tasks", async ({ request }) => {
+  const req = await request.json();
+
+  const sortedTasks = allTasks.sort((a, b) => b.id - a.id);
+
+  const task = {
+    id: sortedTasks[0].id + 1,
+    title: req.title,
+  };
+
+  allTasks.push(task);
+
+  return HttpResponse.json({ task: task }, { status: 201 });
+});
+
+export const handlers = [get, post];
