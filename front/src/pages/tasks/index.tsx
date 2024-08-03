3f65ec9a1f95cc4ba4eb5f2ea50a33c2f2b6eb48
hizahizi-hizumi <hizahizihizumi@gmail.com>
2024-08-03 22:27:53 +0900

    feat: 作成成功時のメッセージをトーストに変更

--- front/src/pages/tasks/index.tsx
+++ front/src/pages/tasks/index.tsx
@@ -1,14 +1,15 @@
 import { useState } from "react";
 import type React from "react";
 
+import { Alert, Snackbar } from "@mui/material";
+
 import { CreateTaskModal } from "./_components/CreateTaskModal/CreateTaskModal";
 import { TaskList } from "./_components/TaskList/TaskList";
 import { useTasks } from "./hooks/useTasks";
 import type { TaskParams } from "./types/taskParams";
 
 export default function Tasks(): React.JSX.Element {
-  const { tasks, error, isLoading, api } = useTasks();
-  const { createdTask, createError } = api.create;
+  const { tasks, error, isLoading } = useTasks();
 
   const [open, setOpen] = useState(false);
 
@@ -20,7 +21,16 @@ export default function Tasks(): React.JSX.Element {
     setOpen(false);
   }
 
-  function onSuccess(_: TaskParams) {
+  const [isSuccess, setIsSuccess] = useState(false);
+  const [createdTask, setCreatedTask] = useState<TaskParams | null>(null);
+
+  function handleSnackbarClose() {
+    setIsSuccess(false);
+  }
+
+  function onSuccess(task: TaskParams) {
+    setIsSuccess(true);
+    setCreatedTask(task);
     handleClose();
   }
 
@@ -37,8 +47,22 @@ export default function Tasks(): React.JSX.Element {
       <button type="button" onClick={handleOpen}>
         タスク作成
       </button>
-      {createdTask && !createError && <p>Task created: {createdTask.title}</p>}
       <TaskList tasks={tasks} />
+
+      <Snackbar
+        open={isSuccess}
+        autoHideDuration={5000}
+        onClose={handleSnackbarClose}
+        anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+      >
+        <Alert
+          onClose={handleSnackbarClose}
+          severity="success"
+          sx={{ width: "100%" }}
+        >
+          {createdTask?.title} を作成しました
+        </Alert>
+      </Snackbar>
 
       <CreateTaskModal
         isOpen={open}
