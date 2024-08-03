2ef3105b44075ed5646a68c2374b85f6d11d86b2
hizahizi-hizumi <hizahizihizumi@gmail.com>
2024-08-03 20:44:34 +0900

    feat: タスク作成をモーダル化

--- front/src/pages/tasks/index.tsx
+++ front/src/pages/tasks/index.tsx
@@ -1,4 +1,13 @@
+import { useState } from "react";
 import type React from "react";
+
+import {
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+} from "@mui/material";
 
 import { CreateTaskForm } from "./_components/CreateTaskForm/CreateTaskForm";
 import { TaskList } from "./_components/TaskList/TaskList";
@@ -9,8 +18,19 @@ export default function Tasks(): React.JSX.Element {
   const { tasks, error, isLoading, api } = useTasks();
   const { createTask, isMutating, createdTask, createError } = api.create;
 
+  const [open, setOpen] = useState(false);
+
+  function handleOpen() {
+    setOpen(true);
+  }
+
+  function handleClose() {
+    setOpen(false);
+  }
+
   function onSubmit(data: TaskParams) {
     createTask(data);
+    handleClose();
   }
 
   const canSubmit = !isMutating;
@@ -25,10 +45,22 @@ export default function Tasks(): React.JSX.Element {
   return (
     <>
       <h1>Tasks</h1>
+      <button onClick={handleOpen}>タスク作成</button>
       {createError && <p>{createError.message}</p>}
       {(createdTask && !createError) && <p>Task created: {createdTask.title}</p>}
-      <CreateTaskForm canSubmit={canSubmit} onSubmit={onSubmit} />
       <TaskList tasks={tasks} />
+
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>タスク作成</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            <CreateTaskForm canSubmit={canSubmit} onSubmit={onSubmit} />
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <button onClick={handleClose}>Close</button>
+        </DialogActions>
+      </Dialog>
     </>
   );
 }
