757c33f829909c5514004113f5e083120124207b
hizahizi-hizumi <hizahizihizumi@gmail.com>
2024-08-03 18:34:45 +0900

    refactor: 他ファイルと記法を統一

--- front/src/hooks/useGet.ts
+++ front/src/hooks/useGet.ts
@@ -1,14 +1,18 @@
 import useSWR from "swr";
 
 async function fetcher<T>(url: string): Promise<T> {
-  return fetch(url).then((res) => res.json());
+  const res = await fetch(url);
+
+  return res.json();
 }
 
-export function useGet<T>(url: string): {
-  data: T | undefined;
+interface UseGetParams {
+  data: any;
   error: Error | null;
   isLoading: boolean;
-} {
+}
+
+export function useGet<T>(url: string): UseGetParams {
   const { data, error, isLoading } = useSWR<T>(url, fetcher);
 
   return { data, error, isLoading };
