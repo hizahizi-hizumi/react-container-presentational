df3c6264fcb5a0c8b27bb33a3af52f55082eb02a
hizahizi-hizumi <hizahizihizumi@gmail.com>
2024-08-04 14:05:08 +0900

    feat: put エンドポイントのモック作成

---
+++ front/src/mocks/tasks/put.ts
@@ -0,0 +1,63 @@
+import { http, HttpResponse } from "msw";
+
+import type { Task } from "@/types/task";
+import { getTasks, updateTask } from ".";
+
+type PutTasksParams = {
+  id: string;
+};
+
+type PutTasksRequestBody = {
+  title: string;
+};
+
+type PutTasksResponseBody = {
+  data: Task | null;
+  message: string;
+};
+
+export const put = http.put<
+  PutTasksParams,
+  PutTasksRequestBody,
+  PutTasksResponseBody,
+  "/tasks/:id"
+>("/tasks/:id", async ({ params, request }) => {
+  // return HttpResponse.error();
+  const req = await request.json();
+
+  const tasks = getTasks();
+
+  const isTaskExist = tasks.some((task) => task.id === Number(params.id));
+
+  if (!isTaskExist) {
+    return HttpResponse.json(
+      {
+        data: null,
+        message: `Task id ${params.id} is not exists`,
+      },
+      { status: 404 },
+    );
+  }
+
+  const taskTitles = tasks.map((task) => task.title);
+  const isParamsInvalid = taskTitles.includes(req.title);
+
+  if (isParamsInvalid) {
+    return HttpResponse.json(
+      {
+        data: null,
+        message: `Task with title "${req.title}" already exists`,
+      },
+      { status: 400 },
+    );
+  }
+
+  const task = {
+    id: Number(params.id),
+    title: req.title,
+  };
+
+  updateTask(task);
+
+  return HttpResponse.json({ data: task, message: "" }, { status: 200 });
+});
