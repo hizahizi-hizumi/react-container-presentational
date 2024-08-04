f41545330aa93b17c8c6dff769168214ea5490a9
hizahizi-hizumi <hizahizihizumi@gmail.com>
2024-08-04 14:22:01 +0900

    feat: タスク編集フォーム作成

---
+++ front/src/pages/tasks/_components/UpdateTaskForm/UpdateTaskForm.tsx
@@ -0,0 +1,39 @@
+import { useForm } from "react-hook-form";
+
+import { useTasks } from "@/pages/tasks/hooks/useTasks";
+import type { TaskParams } from "@/pages/tasks/types/taskParams";
+import type { Task } from "@/types/task";
+
+interface UpdateTaskFormProps {
+  formId: string;
+  onSuccess: (task: TaskParams) => void;
+  task: Task | null;
+}
+
+export function UpdateTaskForm(props: UpdateTaskFormProps): React.JSX.Element {
+  const { formId, onSuccess, task } = props;
+
+  const { register, handleSubmit } = useForm<TaskParams>();
+
+  const { api } = useTasks();
+  const { updateTask, updateError } = api.update;
+
+  async function onSubmit(taskParams: TaskParams) {
+    try {
+      await updateTask(task?.id, taskParams);
+      onSuccess(taskParams);
+    } catch (e) {
+      // onSuccess が呼ばれないように必要
+    }
+  }
+
+  return (
+    <>
+      {updateError && <p>{updateError.message}</p>}
+      <form id={formId} onSubmit={handleSubmit(onSubmit)}>
+        <label>Title</label>
+        <input defaultValue={task?.title} {...register("title")} />
+      </form>
+    </>
+  );
+}
