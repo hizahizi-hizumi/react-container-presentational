f47d425780e19648afc1c8d65987946c0fe19039
hizahizi-hizumi <hizahizihizumi@gmail.com>
2024-08-04 21:39:56 +0900

    remove: 不要ファイル削除

--- front/src/pages/_index.test.jsx
+++
@@ -1,9 +0,0 @@
-import Home from "./index";
-
-describe("Home", () => {
-  it("Home が表示されること", () => {
-    render(<Home />);
-
-    expect(screen.getByText("Home")).toBeInTheDocument();
-  });
-});
