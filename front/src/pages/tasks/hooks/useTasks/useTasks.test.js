dbb2ef809fd1619f20f87de15d5144df1e78cb09
hizahizi-hizumi <hizahizihizumi@gmail.com>
2024-08-04 22:30:40 +0900

    test: useTasks テスト追加

---
+++ front/src/pages/tasks/hooks/useTasks/useTasks.test.js
@@ -0,0 +1,111 @@
+import { http, HttpResponse } from "msw";
+import { setupServer } from "msw/node";
+import { mutate } from "swr";
+
+import { ENDPOINT } from "./ENDPOINT";
+import { useTasks } from "./useTasks";
+
+const server = setupServer();
+
+beforeAll(() => server.listen());
+afterEach(() => {
+  server.resetHandlers();
+  mutate(ENDPOINT, undefined, true); // useSWR のキャッシュをリセット
+});
+afterAll(() => server.close());
+
+function setupResponse(data, message, status) {
+  server.use(
+    http.get(ENDPOINT, () => {
+      return HttpResponse.json({ data, message }, { status });
+    }),
+  );
+}
+
+function setupNetworkError() {
+  server.use(
+    http.get(ENDPOINT, () => {
+      return HttpResponse.error();
+    }),
+  );
+}
+
+describe("useTasks", () => {
+  describe("正常時", () => {
+    const tasks = [{ id: 1, title: "Test Task" }];
+
+    beforeEach(() => {
+      setupResponse({ tasks }, "", 200);
+    });
+
+    it("タスクの取得が成功すること", async () => {
+      const { result } = renderHook(() => useTasks());
+
+      expect(result.current).toEqual({
+        tasks: [],
+        error: undefined,
+        isLoading: true,
+      });
+
+      await waitFor(() => {
+        expect(result.current).toEqual({
+          tasks,
+          error: undefined,
+          isLoading: false,
+        });
+      });
+    });
+  });
+
+  describe("異常時", () => {
+    describe("リクエストでエラーが発生したとき", () => {
+      const errorMessage = "server error";
+
+      beforeEach(() => {
+        setupResponse(undefined, errorMessage, 500);
+      });
+
+      it("エラーを返すこと", async () => {
+        const { result } = renderHook(() => useTasks());
+
+        expect(result.current).toEqual({
+          tasks: [],
+          error: undefined,
+          isLoading: true,
+        });
+
+        await waitFor(() => {
+          expect(result.current).toEqual({
+            tasks: [],
+            error: new Error(errorMessage),
+            isLoading: false,
+          });
+        });
+      });
+    });
+
+    describe("ネットワークでエラーが発生したとき", () => {
+      beforeEach(() => {
+        setupNetworkError();
+      });
+
+      it("ネットワークエラーを返すこと", async () => {
+        const { result } = renderHook(() => useTasks());
+
+        expect(result.current).toEqual({
+          tasks: [],
+          error: undefined,
+          isLoading: true,
+        });
+
+        await waitFor(() => {
+          expect(result.current).toEqual({
+            tasks: [],
+            error: new TypeError("Failed to fetch"),
+            isLoading: false,
+          });
+        });
+      });
+    });
+  });
+});
