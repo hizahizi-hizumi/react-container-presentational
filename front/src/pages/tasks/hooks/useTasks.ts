48bec9998653ba20219d4738c835b5802c9f91d3
hizahizi-hizumi <hizahizihizumi@gmail.com>
2024-08-04 14:21:38 +0900

    feat: タスク更新をタスクカスタムフックに追加

--- front/src/pages/tasks/hooks/useTasks.ts
+++ front/src/pages/tasks/hooks/useTasks.ts
@@ -1,5 +1,6 @@
 import { useGet } from "@/hooks/useGet";
 import { usePost } from "@/hooks/usePost";
+import { usePut } from "@/hooks/usePut";
 import type { TaskParams } from "@/pages/tasks/types/taskParams";
 import type { Task } from "@/types/task";
 
@@ -12,9 +13,15 @@ interface UseTasksReturns {
   api: {
     create: {
       createTask: (params: TaskParams) => Promise<void>;
-      isMutating: boolean;
+      isCreating: boolean;
       createdTask: Task | null;
       createError: Error | null;
+    };
+    update: {
+      updateTask: (id: number | undefined, params: TaskParams) => Promise<void>;
+      isUpdating: boolean;
+      updatedTask: Task | undefined;
+      updateError: Error | undefined;
     };
   };
 }
@@ -29,7 +36,7 @@ export function useTasks(): UseTasksReturns {
 
   const {
     trigger: createTaskTrigger,
-    isMutating,
+    isMutating: isCreating,
     data: createdTask,
     error: createError,
   } = usePost<Task>(ENDPOINT);
@@ -38,10 +45,28 @@ export function useTasks(): UseTasksReturns {
     await createTaskTrigger(params);
   }
 
+  const {
+    trigger: updateTaskTrigger,
+    isMutating: isUpdating,
+    data: updatedTask,
+    error: updateError,
+  } = usePut<TaskParams, Task>(ENDPOINT);
+
+  async function updateTask(id: number | undefined, params: TaskParams) {
+    if (id !== undefined) {
+      await updateTaskTrigger({ id, params });
+    } else {
+      throw new Error("ID is undefined");
+    }
+  }
+
   return {
     tasks,
     error,
     isLoading,
-    api: { create: { createTask, isCreating, createdTask, createError } },
+    api: {
+      create: { createTask, isCreating, createdTask, createError },
+      update: { updateTask, isUpdating, updatedTask, updateError },
+    },
   };
 }
