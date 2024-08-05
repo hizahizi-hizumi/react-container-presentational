08778140f086367f6bdf6b671fc973f57ed4169a
hizahizi-hizumi <hizahizihizumi@gmail.com>
2024-08-05 20:24:21 +0900

    remove: リプレイスして不要になったカスタムフックを削除

--- front/src/pages/tasks/hooks/useTasks/index.ts
+++ front/src/pages/tasks/hooks/useTasks/index.ts
@@ -1,7 +1,6 @@
 import { useCreateTask } from "./useCreateTask";
 import { useDeleteTask } from "./useDeleteTask";
-import { useTaskAPI } from "./useTaskAPI";
 import { useTasks } from "./useTasks";
 import { useUpdateTask } from "./useUpdateTask";
 
-export { useTasks, useTaskAPI, useCreateTask, useUpdateTask, useDeleteTask };
+export { useTasks, useCreateTask, useUpdateTask, useDeleteTask };
