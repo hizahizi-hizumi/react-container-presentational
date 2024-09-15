0af9e0bf82a2fca5a2bde27bc1811acd0cbd055b
hizahizi-hizumi <hizahizihizumi@gmail.com>
2024-09-15 17:55:24 +0900

    refactor: Flagment を使用するように変更

--- front/src/pages/index.tsx
+++ front/src/pages/index.tsx
@@ -2,8 +2,8 @@ import { Link } from "react-router-dom";
 
 export default function Home(): JSX.Element {
   return (
-    <div>
+    <>
       <Link to="/tasks">Tasks</Link>
-    </div>
+    </>
   );
 }
